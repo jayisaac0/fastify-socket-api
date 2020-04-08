53be6d196beebdd6fa5385eeca617b9607f39f13
jayisaac0 <jayisaac0@gmail.com>
2020-04-08 19:52:45 +0300

    [socket api]

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,6 +1,5 @@
 import * as fastify from 'fastify';
 import * as WebSocket from 'ws';
-
 import Responses from '../utils/responses';
 require('../models/user.model')();
 import {RouteSchema} from '../@types/routeschema';
@@ -15,39 +14,102 @@ export default (app: fastify.FastifyInstance) => {
         const wss = new WebSocket.Server(app);
 
 
-        wss.on('connection', (ws: WebSocket) => {
+        wss.on('connection', (socket: WebSocket) => {
 
-            ws.isAlive = true;
+            socket.isAlive = true;
 
-            ws.on('pong', () => {
-                ws.isAlive = true;
+            socket.on('pong', () => {
+                socket.isAlive = true;
             });
 
             //connection is up, let's add a simple simple event
-            ws.on('message', (message: string) => {
-                console.log('received: %s', message);
-
-                const broadcastRegex = /^broadcast\:/;
+            socket.on('message', (message: string) => {
 
 
+                const payload = JSON.parse(message);
 
+                switch (payload.url) {
+                    case '/users/create':
+                        
+                        const { first_name, last_name, email, password } = payload.body;
 
+                        const action = 'users';
 
-                if (broadcastRegex.test(message)) {
-                    message = message.replace(broadcastRegex, '');
-                    wss.clients
-                        .forEach(client => {
-                            if (client != ws) {
-                                client.send(`Hello, broadcast message -> ${message}`);
-                            }    
-                        });
+                        const userInterface = {
+                            first_name: {type: 'string', minLength: 4},
+                            last_name: {type: 'string', minLength: 4, maxLength: 255 },
+                            email: {type: 'string'},
+                            password: {type: 'string'},
+                        };
                     
-                } else {
-                    ws.send(`Hello, you sent -> ${message}`);
+                        /**
+                         * CREATE ITEM SOCKET ACTION
+                         */
+                        const createUserSchema: RouteSchema = {
+                            tags: [`${action}`], 
+                            summary: ` Create new ${action} endpoint`,
+                            body:  {
+                                type: 'object',
+                                additionalProperties: false,
+                                properties: userInterface,
+                                required: ['first_name', 'last_name', 'email', 'password']
+                            },
+                            response: {
+                                400: {
+                                    type: 'object',
+                                    properties: {
+                                        code: { type: 'number' },
+                                        message: { type: 'string' },
+                                        error: {type: 'string'}
+                                    }        
+                                },
+                                200: {
+                                    type: 'object',
+                                    properties: {
+                                        code: { type: 'number' },
+                                        message: { type: 'string' },
+                                        success: {type: 'string'},
+                                        data: {
+                                            type: 'array', items: {
+                                                type: 'object',
+                                                properties: {
+                                                    ...userInterface
+                                                }
+                                            }
+                                        }
+                                    }  
+                                }
+                            }
+                        }
+                        
+                        socket.send(JSON.stringify(payload.body));
+                        
+                        break;
+                
+                    default:
+                        break;
                 }
+
+
+
+
+                // console.log('received: %s', message);
+                // const broadcastRegex = /^broadcast\:/;
+                // if (broadcastRegex.test(message)) {
+                //     message = message.replace(broadcastRegex, '');
+                //     socket.clients
+                //         .forEach(client => {
+                //             if (client != socket) {
+                //                 client.send(`Hello, broadcast message -> ${message}`);
+                //             }    
+                //         });
+                    
+                // } else {
+                //     socket.send(`Hello, you sent -> ${JSON.stringify({data: message})}`);
+                // }
             });
 
-            ws.on('close', () => {
+            socket.on('close', () => {
                 console.log("I lost a client");
             });
 
